7415f1e821932cce8cbc09a16e00643541a7a21d
Владислав <vladkeda8@gmail.com>
2026-08-08 13:58:57 +0300

    add logs, video

--- src/test/java/tests/TestPotters.java
+++ src/test/java/tests/TestPotters.java
@@ -1,14 +1,21 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import enums.Account;
+import heplers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.BankingPage;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.webdriver;
 import static io.qameta.allure.Allure.addAttachment;
@@ -16,6 +23,25 @@ import static io.qameta.allure.Allure.step;
 
 public class TestPotters extends TestBase {
     BankingPage bankingPage = new BankingPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+//        Configuration.remote = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
     @ParameterizedTest
     @DisplayName("Успешная авторизация пользователя в системе")
@@ -55,7 +81,6 @@ public class TestPotters extends TestBase {
             "Neville Longbottom"
     })
     void testAnnotatedStep(String searchQuery) {
-        SelenideLogger.addListener("allure", new AllureSelenide());
         WebSteps steps = new WebSteps(bankingPage);
 
         steps.openMainPage();
@@ -63,14 +88,12 @@ public class TestPotters extends TestBase {
         steps.takeScreenshot();
         steps.login();
         steps.checkCustomer(searchQuery);
-
     }
 
     @ParameterizedTest
     @DisplayName("Успешная авторизация пользователя в системе")
     @EnumSource(Account.class)
     void customerSuccessLoginTest1(Account account) {
-        SelenideLogger.addListener("allure", new AllureSelenide());
         bankingPage.openCustomerPage()
                 .selectCustomer(account.getCustomerName())
                 .login()
@@ -84,5 +107,13 @@ public class TestPotters extends TestBase {
                 .clickApplyDepositButton()
                 .checkSuccessDeposit()
                 .checkBalance(account.getFinalAmount());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("LastScreenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
